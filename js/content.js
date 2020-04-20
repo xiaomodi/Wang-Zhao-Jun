09cfdc55c8194034380d3d6ae45aa3bef1ed2cd9
xiaomodi <13851416778@163.com>
2020-04-21 00:46:39 +0800

    wangzhaojun

--- js/content.js
+++ js/content.js
@@ -1,20 +1,28 @@
 $(function () {
     $('#content1').click(function() {
-        $('#content2').css('display', 'block')
+        $('#contentOne').css('display', 'block')
         $('#content1').css('display', 'none')
+    })
+    $('#content1').click(function() {
+        $('#contentOne').css('display', 'block')
+        $('#content1').css('display', 'none')
+    })
+    $('#contentOne').click(function() {
+        $('#content2').css('display', 'block')
+        $('#contentOne').css('display', 'none')
     })
     $('#content2').click(function() {
         $('#content3').css('display', 'block')
         $('#content2').css('display', 'none')
     })
     $('#content3').click(function() {
-        $('#content4').css('display', 'block')
+        $('#content5').css('display', 'block')
         $('#content3').css('display', 'none')
     })
-    $('#content4').click(function() {
-        $('#content5').css('display', 'block')
-        $('#content4').css('display', 'none')
-    })
+    // $('#content4').click(function() {
+    //     $('#content5').css('display', 'block')
+    //     $('#content4').css('display', 'none')
+    // })
     $('#content5').click(function() {
         $('#content6').css('display', 'block')
         $('#content5').css('display', 'none')
@@ -32,8 +40,12 @@ $(function () {
         $('#content8').css('display', 'none')
     })
     $('#content9').click(function() {
-        $('#content10').css('display', 'block')
+        $('#contentNine').css('display', 'block')
         $('#content9').css('display', 'none')
+    })
+    $('#contentNine').click(function() {
+        $('#content10').css('display', 'block')
+        $('#contentNine').css('display', 'none')
     })
     $('#content10').click(function() {
         $('#content11').css('display', 'block')
---
+++ js/wenzi..js
@@ -0,0 +1,7 @@
+const text1 = '时光一晃而过，昭君逐渐长成';
+
+console.log(text1.split(''));
+const item = text1.split('');
+item.forEach(i => {
+    console.log(i)
+});
