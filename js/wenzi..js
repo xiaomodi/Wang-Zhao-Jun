faf06852263396d602c6c67a2972b979bb532eaf
xiaomodi <13851416778@163.com>
2020-04-22 19:35:02 +0800

    today

--- js/wenzi..js
+++ js/wenzi..js
@@ -5,3 +5,17 @@ const item = text1.split('');
 item.forEach(i => {
     console.log(i)
 });
+
+const descWrapper = document.getElementById('desc-wrapper')
+
+descWrapper.onclick = () => {
+    showDiv()
+}
+var p1 = document.getElementById('desc1-p')
+function showDiv () {
+    n++;
+    if (n <= 2) {
+        p1.style.visibility = 'visible'
+        setTimeout('showDiv', 1000)
+    }   
+}
