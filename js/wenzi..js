faba5dea14ebcd562c430fc171fdda8c9c047841
xiaomodi <13851416778@163.com>
2020-04-24 01:03:25 +0800

    ten

--- js/wenzi..js
+++ js/wenzi..js
@@ -1,6 +1,7 @@
 const t1 = document.getElementById('t1')
 const t2 = document.getElementById('t2')
 const t3 = document.getElementById('t3')
+const audio = document.getElementById('audio1')
 window.onload = () => {
     setTimeout(() => {
         t1.style.opacity = 1
@@ -14,6 +15,8 @@ window.onload = () => {
         t3.style.opacity = 1
         t3.style.transition = 'all .6s linear'
     }, 2600)
+    audio.defaultMuted = true
+    console.log(audio)
 }
 
 // showT2 = () => {
@@ -26,15 +29,15 @@ const content1 = document.getElementById('content1')
 const p1 = document.getElementById('p1')
 const p2 = document.getElementById('p2')
 content1.onclick = () => {
-    setTimeout(changStyle, 1000)
+    setTimeout(changStyle, 2000)
 }
 
 const index = 1
 changStyle = () =>{
     p1.style.visibility = 'visible'
     p1.style.opacity = 1
-    p1.style.transition = 'opacity 1s linear'
-    setTimeout(change, 1500)
+    p1.style.transition = 'all 1s linear'
+    setTimeout(change, 2000)
 }
 
 change = () => {
@@ -42,4 +45,138 @@ change = () => {
     p2.style.visibility = 'visible'
     p2.style.opacity = 1
     p2.style.transition = 'opacity 1s linear'
+}
+
+const content2 = document.getElementById('content2')
+const TP1 = document.getElementById('third-p1')
+const TP2 = document.getElementById('third-p2')
+
+content2.onclick = () => {
+    setTimeout(() => {
+        TP1.style.opacity = 1
+        TP1.style.transition = 'all 2s linear'
+        setTimeout(TPShow, 2000)
+    }, 1500)
+}
+TPShow = () => {
+    TP2.style.opacity = 1
+    TP2.style.transition = 'all 2s linear'
+}
+
+const content3 = document.getElementById('content3')
+const FP1 = document.getElementById('fivth-p1')
+const FP2 = document.getElementById('fivth-p2')
+
+content3.onclick = () => {
+    setTimeout(() => {
+        FP1.style.opacity = 1
+        FP1.style.transition = 'all 2s linear'
+        setTimeout(FPShow, 2000)
+    }, 1500)
+}
+
+FPShow = () => {
+    FP2.style.opacity = 1
+    FP2.style.transition = 'all 2s linear'
+}
+
+
+const content5 = document.getElementById('content5')
+const SP1 = document.getElementById('sixth-p1')
+const SP2 = document.getElementById('sixth-p2')
+
+content5.onclick = () => {
+    setTimeout(() => {
+        SP1.style.opacity = 1
+        SP1.style.transition = 'all 2s linear'
+        setTimeout(SPShow, 2000)
+    }, 1500)
+}
+
+SPShow = () => {
+    SP2.style.opacity = 1
+    SP2.style.transition = 'all 2s linear'
+}
+
+const content6 = document.getElementById('content6')
+const SEP1 = document.getElementById('seven-p1')
+const SEP2 = document.getElementById('seven-p2')
+
+content6.onclick = () => {
+    setTimeout(() => {
+        SEP1.style.opacity = 1
+        SEP1.style.transition = 'all 2s linear'
+        setTimeout(SEPShow, 2000)
+    }, 1500)
+}
+
+SEPShow = () => {
+    SEP2.style.opacity = 1
+    SEP2.style.transition = 'all 2s linear'
+}
+
+const content7 = document.getElementById('content7')
+const EP1 = document.getElementById('eight-p1')
+
+content7.onclick = () => {
+    setTimeout(() => {
+        EP1.style.opacity = 1
+        EP1.style.transition = 'all 2s linear'
+    }, 1800)
+}
+
+const content8 = document.getElementById('content8')
+const NP1 = document.getElementById('nine-p1')
+
+content8.onclick = () => {
+    setTimeout(() => {
+        NP1.style.opacity = 1
+        NP1.style.transition = 'all 2s linear'
+    }, 1800)
+}
+
+const b = document.getElementById('content9')
+const CNP1 = document.getElementById('CNP1')
+const CNP2 = document.getElementById('CNP2')
+const CNP3 = document.getElementById('CNP3')
+const CNP4 = document.getElementById('CNP4')
+const CNP5 = document.getElementById('CNP5')
+const CNP6 = document.getElementById('CNP6')
+const CNP7 = document.getElementById('CNP7')
+b.onclick=()=>{
+    setTimeout(() => {
+        CNP1.style.opacity = 1
+        CNP1.style.transition = 'all 1s linear'
+        setTimeout(CNP2Show, 1200)
+    }, 1200)
+}
+CNP2Show = () => {
+    CNP2.style.opacity = 1
+    CNP2.style.transition = 'all 1s linear'
+    setTimeout(CNP3Show, 1200)
+}
+CNP3Show = () => {
+    CNP3.style.opacity = 1
+    CNP3.style.transition = 'all 1s linear'
+    setTimeout(CNP4Show, 1200)
+}
+CNP4Show = () => {
+    CNP4.style.opacity = 1
+    CNP4.style.transition = 'all 1s linear'
+    setTimeout(CNP5Show, 1200)
+}
+CNP5Show = () => {
+    CNP5.style.opacity = 1
+    CNP5.style.transition = 'all 1s linear'
+    setTimeout(NP6Show, 1200)
+}
+NP6Show = () => {
+    CNP6.style.opacity = 1
+    CNP6.style.transition = 'all 1s linear'
+    setTimeout(CNP7Show, 1200)
+}
+CNP7Show = () => {
+    CNP7.style.opacity = 1
+    CNP7.style.transition = 'all 1s linear'
+    setTimeout(CNP3Show, 1200)
 }
